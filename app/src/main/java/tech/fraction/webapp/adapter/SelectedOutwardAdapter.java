4ae45f42b8defd940fa1162027157296079f776c
gautam <suranigautam98@gmail.com>
2019-02-14 17:02:32 +0530

    Merge branch 'aashita' of https://github.com/gautamsurani/WebApp into gautam
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/tech/fraction/webapp/fragment/OutwordListFragment.java

--- app/src/main/java/tech/fraction/webapp/adapter/SelectedOutwardAdapter.java
+++ app/src/main/java/tech/fraction/webapp/adapter/SelectedOutwardAdapter.java
@@ -7,20 +7,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import tech.fraction.webapp.R;
-import tech.fraction.webapp.model.InvoiceDetails;
 import tech.fraction.webapp.model.OutwardDetails;
-import tech.fraction.webapp.util.Utils;
-
 
 public class SelectedOutwardAdapter extends RecyclerView.Adapter<SelectedOutwardAdapter.ViewHolder> {
-
 
     private LayoutInflater inflater;
     private List<OutwardDetails> outwardDetails = new ArrayList<>();
@@ -29,12 +24,11 @@ public class SelectedOutwardAdapter extends RecyclerView.Adapter<SelectedOutward
     public SelectedOutwardAdapter(Context context) {
         inflater = LayoutInflater.from(context);
         this.context = context;
-
     }
 
-    private SelectedOutwardAdapter.OnClickListener onClickListener;
+    private OnClickListener onClickListener;
 
-    public void setOnItemClickListener(SelectedOutwardAdapter.OnClickListener onClickListener) {
+    public void setOnItemClickListener(OnClickListener onClickListener) {
         this.onClickListener = onClickListener;
     }
 
@@ -48,16 +42,14 @@ public class SelectedOutwardAdapter extends RecyclerView.Adapter<SelectedOutward
 
     @NonNull
     @Override
-    public SelectedOutwardAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
         View view;
-
         view = inflater.inflate(R.layout.row_selected_outward, parent, false);
-
-        return new SelectedOutwardAdapter.ViewHolder(view);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull SelectedOutwardAdapter.ViewHolder holder, final int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         holder.tvItemName.setText(outwardDetails.get(position).getItemName());
         holder.imgDelete.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,7 +59,6 @@ public class SelectedOutwardAdapter extends RecyclerView.Adapter<SelectedOutward
         });
     }
 
-
     @Override
     public int getItemCount() {
         return outwardDetails.size();
@@ -75,15 +66,12 @@ public class SelectedOutwardAdapter extends RecyclerView.Adapter<SelectedOutward
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView tvItemName, tvItemUnit, tvStock, tvOutNo, tv0utDate, tvLocation;
-
         ImageView imgDelete;
 
         public ViewHolder(@NonNull View item) {
             super(item);
             tvItemName = item.findViewById(R.id.tvItemName);
             imgDelete = item.findViewById(R.id.imgDelete);
-
         }
     }
 }
- 
